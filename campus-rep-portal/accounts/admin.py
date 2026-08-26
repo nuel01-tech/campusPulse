bf0611ea140771883fd6f64e60dd5c9086aad6c6
unknown <oluwasegundekoya@gmail.com>
2026-08-26 22:27:01 +0100

    Extend UserAdmin to show custom fields like role and department

--- campus-rep-portal/accounts/admin.py
+++ campus-rep-portal/accounts/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Department
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('CampusPulse Info', {
+            'fields': ('role', 'department', 'level', 'matric_number', 'phone_number', 'enable_wakeup_calls')
+        }),
+    )
+    list_display = ('username', 'email', 'role', 'department', 'level', 'is_staff')
+
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Department)
