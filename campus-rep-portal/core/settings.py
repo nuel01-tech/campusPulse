5cc3c761dae428f59224b3fc98af251cc4653527
unknown <oluwasegundekoya@gmail.com>
2026-08-29 11:50:28 +0100

    Switch to non-manifest Whitenoise storage to fix admin static crash

--- campus-rep-portal/core/settings.py
+++ campus-rep-portal/core/settings.py
@@ -146,11 +146,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-WHITENOISE_MANIFEST_STRICT = False
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
