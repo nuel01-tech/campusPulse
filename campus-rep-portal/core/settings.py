a8a3a06a16ca2514fcaf06aee2b80e6706fddac8
unknown <oluwasegundekoya@gmail.com>
2026-08-29 11:52:50 +0100

    Use plain static storage to avoid whitenoise post-processing crashes

--- campus-rep-portal/core/settings.py
+++ campus-rep-portal/core/settings.py
@@ -146,10 +146,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
